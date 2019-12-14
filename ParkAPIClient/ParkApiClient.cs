7b031d725e323eb601ee0c061047b9431ab03bf2
Emanuel Vintilă <eml.vintila@gmail.com>
2019-12-14 17:02:03 +0200

    Created .NET class library ParkAPIClient

---
+++ ParkAPIClient/ParkApiClient.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+
+namespace ParkAPIClient
+{
+    public class ParkApiClient
+    {
+        private string Host { get; }
+        
+        public ParkApiClient(string host)
+        {
+            Host = host;
+        }
+
+        public async Task<bool> UpdateCurrentAsync(int id, int current)
+        {
+            HttpWebRequest request = WebRequest.CreateHttp($"{Host}/api/lots/update/${id}/{current}");
+            request.Method = WebRequestMethods.Http.Post;
+            HttpWebResponse response = (HttpWebResponse) await request.GetResponseAsync();
+            
+            return response.StatusCode == HttpStatusCode.OK;
+        }
+    }
+}
