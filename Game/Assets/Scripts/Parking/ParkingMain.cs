701e500e603c712624ba7f9df7da8bcb7713bd6a
Cirstea Andrei <andrey0103@yahoo.com>
2019-12-14 15:08:40 +0200

    Overall folder.

---
+++ Game/Assets/Scripts/Parking/ParkingMain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Parking
+{
+	public class ParkingMain : MonoBehaviour
+	{
+		public int maxSlots;
+		public int columns;
+		[FormerlySerializedAs("rounds")] public int rows;
+		
+		public List<int> slots;
+
+		void Awake()
+		{
+			slots = new List<int>();
+		}
+		
+		
+	}
+}
---
+++ Game/Assets/Scripts/Parking/ParkingPlatform.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+// ReSharper disable PossibleLossOfFraction
+
+namespace Parking
+{
+	public class ParkingPlatform : MonoBehaviour
+	{
+		public float speed = 50f;
+
+		bool platformMoving = false;
+		ParkingMain parkingMain;
+
+		void Awake()
+		{
+			parkingMain = GetComponentInParent<ParkingMain>();
+		}
+
+		public void MovePlatformToSlot(int slot)
+		{
+			int pack = slot / 4;
+			MovePlatformToPack(pack);
+
+			int count = slot % 4;
+			switch (count)
+			{
+				case 1:
+					PlatformMove(Vector3.back);
+					PlatformDropVehicle(Vector3.right);
+					break;
+				case 2:
+					PlatformMove(Vector3.back * 2);
+					PlatformDropVehicle(Vector3.right);
+					break;
+				case 3:
+					PlatformMove(Vector3.back * 3);
+					PlatformMove(Vector3.right * 2);
+					PlatformDropVehicle(Vector3.forward);
+					break;
+				case 0:
+					PlatformMove(Vector3.back * 3);
+					PlatformMove(Vector3.right * 3);
+					PlatformMove(Vector3.forward * 2);
+					PlatformDropVehicle(Vector3.left);
+					break;
+			}
+		}
+
+		void PlatformDropVehicle(Vector3 right)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void MovePlatformToPack(int pack)
+		{
+			PlatformMove(Vector3.back * (pack / parkingMain.columns));
+			PlatformMove(Vector3.right * ((pack % parkingMain.columns - 1 + parkingMain.columns) % parkingMain.columns));
+		}
+
+		public void PlatformMove(Vector3 movement)
+		{
+			StartCoroutine(nameof(PlatformMoveCoroutine), transform.position + movement);
+		}
+
+		IEnumerator PlatformMoveCoroutine(Vector3 targetPosition)
+		{
+			if (platformMoving)
+				yield return new WaitForSeconds(0.1f);
+
+			platformMoving = true;
+			Vector3 direction = targetPosition - transform.position;
+			while (!Mathf.Approximately(0, direction.magnitude))
+			{
+				transform.Translate(Time.deltaTime * speed * direction.normalized);
+
+				yield return new WaitForFixedUpdate();
+			}
+
+			platformMoving = false;
+		}
+	}
+}
